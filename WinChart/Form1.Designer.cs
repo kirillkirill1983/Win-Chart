d61d50f3fb9b7ec3ee65ed46df0f0e16efc9e5bd
Кирилл <57317953+kirillkirill1983@users.noreply.github.com>
2020-10-06 17:26:37 +0300

    Excel table
    building a graph based on an Excel table

---
+++ WinChart/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WinChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea17 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend17 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series17 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea18 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend18 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series18 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea19 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend19 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea20 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend20 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series20 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea17.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea17);
+            legend17.Name = "Legend1";
+            this.chart1.Legends.Add(legend17);
+            this.chart1.Location = new System.Drawing.Point(-1, 1);
+            this.chart1.Name = "chart1";
+            series17.ChartArea = "ChartArea1";
+            series17.Legend = "Legend1";
+            series17.Name = "Series1";
+            this.chart1.Series.Add(series17);
+            this.chart1.Size = new System.Drawing.Size(1266, 197);
+            this.chart1.TabIndex = 700;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(22, 857);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить График №1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart2
+            // 
+            this.chart2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea18.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea18);
+            legend18.Name = "Legend1";
+            this.chart2.Legends.Add(legend18);
+            this.chart2.Location = new System.Drawing.Point(-1, 204);
+            this.chart2.Name = "chart2";
+            series18.ChartArea = "ChartArea1";
+            series18.Legend = "Legend1";
+            series18.Name = "Series1";
+            this.chart2.Series.Add(series18);
+            this.chart2.Size = new System.Drawing.Size(1266, 196);
+            this.chart2.TabIndex = 701;
+            this.chart2.Text = "chart2";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(174, 857);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 37);
+            this.button2.TabIndex = 702;
+            this.button2.Text = "Построить График №2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart3
+            // 
+            this.chart3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea19.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea19);
+            legend19.Name = "Legend1";
+            this.chart3.Legends.Add(legend19);
+            this.chart3.Location = new System.Drawing.Point(-1, 406);
+            this.chart3.Name = "chart3";
+            series19.ChartArea = "ChartArea1";
+            series19.Legend = "Legend1";
+            series19.Name = "Series1";
+            this.chart3.Series.Add(series19);
+            this.chart3.Size = new System.Drawing.Size(1266, 195);
+            this.chart3.TabIndex = 703;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            this.chart4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea20.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea20);
+            legend20.Name = "Legend1";
+            this.chart4.Legends.Add(legend20);
+            this.chart4.Location = new System.Drawing.Point(-1, 607);
+            this.chart4.Name = "chart4";
+            series20.ChartArea = "ChartArea1";
+            series20.Legend = "Legend1";
+            series20.Name = "Series1";
+            this.chart4.Series.Add(series20);
+            this.chart4.Size = new System.Drawing.Size(1266, 195);
+            this.chart4.TabIndex = 704;
+            this.chart4.Text = "chart4";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(326, 857);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 37);
+            this.button3.TabIndex = 705;
+            this.button3.Text = "Построить График №3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(478, 857);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 37);
+            this.button4.TabIndex = 706;
+            this.button4.Text = "Построить График №4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1290, 906);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "v";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WinChart/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WinChart
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application ExcelApp = new Excel.Application();// Excel 
+        Excel.Workbook ExcelWB; //work book
+        Excel.Worksheet ExcelShit;// list Excel
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExcelWB = ExcelApp.Workbooks.Open(@"D:\Bks\raw_0.xlsx");
+            ExcelShit = ExcelWB.Worksheets["Лист1"];
+            int totalColumns = ExcelShit.UsedRange.Columns.Count;
+            int totalRows = ExcelShit.UsedRange.Rows.Count;
+            Range dataRangeTemperatura = null;
+            Range dataRangeTime = null;
+            List<string> CellsListTemperatura = new List<string>();
+            List<string> CellListTime = new List<string>();
+           
+
+            for (int row = 1; row < totalRows; row++) // 
+            {
+                dataRangeTemperatura = (Range)ExcelShit.Cells[row, 2];
+                dataRangeTime = (Range)ExcelShit.Cells[row, 1];
+                CellsListTemperatura.Add(dataRangeTemperatura.Value);
+                CellListTime.Add(dataRangeTime.Value);
+            }
+
+            
+            
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+
+            string[] textTemperatura = CellsListTemperatura.ToArray();
+            int[] grafTemperatura = new int[totalRows];
+            chart1.ChartAreas[0].AxisY.Minimum=1000;
+            
+            for (int i = 1; i < totalRows-1; i++)
+            {
+                grafTemperatura[i] = Convert.ToInt32(textTemperatura[i]);
+            }
+            for (int i = 1; i < totalRows-1; i++)
+            {
+
+                chart1.Series[0].Points.AddXY(i, grafTemperatura[i]);
+
+            }
+
+            ExcelApp.Quit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExcelWB = ExcelApp.Workbooks.Open(@"D:\Bks\raw_1.xlsx");
+            ExcelShit = ExcelWB.Worksheets["Лист1"];
+            int totalColumns = ExcelShit.UsedRange.Columns.Count;
+            int totalRows = ExcelShit.UsedRange.Rows.Count;
+            Range dataRangeTemperatura = null;
+            Range dataRangeTime = null;
+            List<string> CellsListTemperatura = new List<string>();
+            List<string> CellListTime = new List<string>();
+
+
+            for (int row = 1; row < totalRows; row++) // 
+            {
+                dataRangeTemperatura = (Range)ExcelShit.Cells[row, 2];
+                dataRangeTime = (Range)ExcelShit.Cells[row, 1];
+                CellsListTemperatura.Add(dataRangeTemperatura.Value);
+                CellListTime.Add(dataRangeTime.Value);
+            }
+
+
+
+            chart2.Series[0].Points.Clear();
+            chart2.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+
+            string[] textTemperatura = CellsListTemperatura.ToArray();
+            int[] grafTemperatura = new int[totalRows];
+            chart2.ChartAreas[0].AxisY.Minimum = 800;
+
+            for (int i = 1; i < totalRows - 1; i++)
+            {
+                grafTemperatura[i] = Convert.ToInt32(textTemperatura[i]);
+            }
+            for (int i = 1; i < totalRows - 1; i++)
+            {
+
+                chart2.Series[0].Points.AddXY(i, grafTemperatura[i]);
+
+            }
+
+            ExcelApp.Quit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ExcelWB = ExcelApp.Workbooks.Open(@"D:\Bks\raw_2.xlsx");
+            ExcelShit = ExcelWB.Worksheets["Лист1"];
+            int totalColumns = ExcelShit.UsedRange.Columns.Count;
+            int totalRows = ExcelShit.UsedRange.Rows.Count;
+            Range dataRangeTemperatura = null;
+            Range dataRangeTime = null;
+            List<string> CellsListTemperatura = new List<string>();
+            List<string> CellListTime = new List<string>();
+
+
+            for (int row = 1; row < totalRows; row++) // 
+            {
+                dataRangeTemperatura = (Range)ExcelShit.Cells[row, 2];
+                dataRangeTime = (Range)ExcelShit.Cells[row, 1];
+                CellsListTemperatura.Add(dataRangeTemperatura.Value);
+                CellListTime.Add(dataRangeTime.Value);
+            }
+
+
+
+            chart3.Series[0].Points.Clear();
+            chart3.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+
+            string[] textTemperatura = CellsListTemperatura.ToArray();
+            int[] grafTemperatura = new int[totalRows];
+            chart3.ChartAreas[0].AxisY.Minimum = 700;
+
+            for (int i = 1; i < totalRows - 1; i++)
+            {
+                grafTemperatura[i] = Convert.ToInt32(textTemperatura[i]);
+            }
+            for (int i = 1; i < totalRows - 1; i++)
+            {
+
+                chart3.Series[0].Points.AddXY(i, grafTemperatura[i]);
+
+            }
+
+            ExcelApp.Quit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ExcelWB = ExcelApp.Workbooks.Open(@"D:\Bks\raw_3.xlsx");
+            ExcelShit = ExcelWB.Worksheets["Лист1"];
+            int totalColumns = ExcelShit.UsedRange.Columns.Count;
+            int totalRows = ExcelShit.UsedRange.Rows.Count;
+            Range dataRangeTemperatura = null;
+            Range dataRangeTime = null;
+            List<string> CellsListTemperatura = new List<string>();
+            List<string> CellListTime = new List<string>();
+
+
+            for (int row = 1; row < totalRows; row++) // 
+            {
+                dataRangeTemperatura = (Range)ExcelShit.Cells[row, 2];
+                dataRangeTime = (Range)ExcelShit.Cells[row, 1];
+                CellsListTemperatura.Add(dataRangeTemperatura.Value);
+                CellListTime.Add(dataRangeTime.Value);
+            }
+
+
+
+            chart4.Series[0].Points.Clear();
+            chart4.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+
+            string[] textTemperatura = CellsListTemperatura.ToArray();
+            int[] grafTemperatura = new int[totalRows];
+            chart4.ChartAreas[0].AxisY.Minimum = 700;
+
+            for (int i = 1; i < totalRows - 1; i++)
+            {
+                grafTemperatura[i] = Convert.ToInt32(textTemperatura[i]);
+            }
+            for (int i = 1; i < totalRows - 1; i++)
+            {
+
+                chart4.Series[0].Points.AddXY(i, grafTemperatura[i]);
+
+            }
+
+            ExcelApp.Quit();
+        }
+    }
+}
